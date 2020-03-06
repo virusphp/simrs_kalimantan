d669bd73f1ea7d5cea60ad31d368d660f8cf4e31
Awaludin <awaludinr830@gmail.com>
2020-03-06 08:19:58 +0700

    change patients

--- app/Http/Controllers/API/PasienController.php
+++ app/Http/Controllers/API/PasienController.php
@@ -9,28 +9,6 @@ use App\Instalasi;
 
 class PasienController extends Controller
 {
-    //
-    // Pasien Registrasi sendiri untuk rawat Inap
-    // TODO:
-    // * pasien mendaftar rawat inap
-    // * pasien mendapatkan no rekam medis yang nantinya dapat digunakan
-    // untuk memesan kamar
-    public function register(Request $request, $login = false)
-    {
-		$validator = Validator::make($request->all(), [
-			'nama_lengkap'  => 'required',
-			'agama'	    => 'required',
-			'tanggal_lahir' => 'required',
-			'penjamin_id'   => 'required',
-			'shift_id'      => -1,
-			'pasien_id'     => '',
-		]);
-
-		$instalasi_rawat_inap = Instalasi::where('instalasi_nama', 'Rawat Inap');
-
-		$request->merge(['instalasi_id' => $instalasi_rawat_inap]);
-    }
-
     public function show()
     {
 	return \Response::json(\App\Pendaftaran::first());
